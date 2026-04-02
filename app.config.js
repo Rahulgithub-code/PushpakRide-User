c9a790408f58b62923a2f3d557f9bac81cc49c20
Rahul Kumar Ram <rahulkr.mits@gmail.com>
2026-04-03 01:29:22 +0530

    Initial commit
    Initial commit

---
+++ app.config.js
@@ -0,0 +1,62 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "PushpakRide-User",
+    slug: "PushpakRide-User",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./src/assets/images/icon.png",
+    scheme: "pushpakrideuser",
+    newArchEnabled: true,
+    userInterfaceStyle: "automatic",
+
+    splash: {
+      image: "./src/assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.pushpakride.user",
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAP_API_KEY,
+      },
+    },
+
+    android: {
+      package: "com.pushpakride.user",
+      adaptiveIcon: {
+        foregroundImage: "./src/assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAP_API_KEY,
+        },
+      },
+    },
+
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./src/assets/images/favicon.png",
+    },
+
+    plugins: [
+      "expo-router",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ]
+    ],
+
+    experiments: {
+      typedRoutes: true,
+    },
+  },
+};
