6edf56599211d8bbfb31d0958be0fe12255b63bc
Rahul Kumar Ram <rahulkr.mits@gmail.com>
2026-04-04 21:41:10 +0530

    update screen names

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,6 +10,12 @@ export default function Layout() {
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
           <Stack.Screen name="role" />
+          <Stack.Screen name="customer/selectlocations" />
+          <Stack.Screen name="customer/ridebooking" />
+          <Stack.Screen name="customer/home" />
+          <Stack.Screen name="customer/auth" />
+          <Stack.Screen name="rider/home" />
+          <Stack.Screen name="rider/auth" />
         </Stack>
       </WSProvider>
     </GestureHandlerRootView>
